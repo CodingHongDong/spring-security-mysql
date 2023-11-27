a5725df42fe4340fb88c69c434fa86a583ac00ed
DESKTOP-8CMUIHA\hong <hongdh94@gmail.com>
2023-11-27 21:44:53 +0900

    20231127

---
+++ src/main/java/com/hong/blog/test/Member.java
@@ -0,0 +1,17 @@
+package com.hong.blog.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+	private int id;
+	private String username;
+	private int password;
+	private String email;
+	
+}
