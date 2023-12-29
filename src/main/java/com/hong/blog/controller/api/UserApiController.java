a43bdc162c06824fb48d0c437f4f801d81269b81
CodingHongDong <hongdh94@gmail.com>
2023-12-29 21:00:48 +0900

    ADD 2023-12-29

--- src/main/java/com/hong/blog/controller/api/UserApiController.java
+++ src/main/java/com/hong/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.hong.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +19,27 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController : save() 호출됨.");
 		// 실제 DB에 insert
 		user.setRole(RoleType.USER);
 		userService.join(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user) {
+		System.out.println("UserApiController : login() 호출됨.");
+		User principal = userService.login(user);
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
--- src/main/java/com/hong/blog/repository/UserRepository.java
+++ src/main/java/com/hong/blog/repository/UserRepository.java
@@ -6,4 +6,8 @@ import com.hong.blog.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	// JPA Naming 전략
+	// SELECT * FROM user WHERE username=?1 AND password=?2
+	User findByUsernameAndPassword(String username, String password);
+	
 }
