2fd5d6b022b408df11529813b83cf24d5412322e
CodingHongDong <hongdh94@gmail.com>
2024-01-04 20:10:29 +0900

    ADD spring security 로그인 확인 / 2024-01-04

--- src/main/java/com/hong/blog/repository/UserRepository.java
+++ src/main/java/com/hong/blog/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.hong.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hong.blog.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	// SELECT * FROM user WHERE username = 1?;
+	Optional<User> findByUsername(String username);
 }
 
 
